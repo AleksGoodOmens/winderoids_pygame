fa07879aab7ed77fea0841c50576992787991f1a
Alex G <agsfinks@gmail.com>
2022-05-28 01:38:29 +0100

    version 2.0

--- winderoids.py
+++ winderoids.py
@@ -43,7 +43,7 @@ font_name = pygame.font.match_font('monospace')
 
 def draw_text(surf, text, size, x, y):
     font = pygame.font.Font(font_name, size)
-    text_surface = font.render(text, True, WHITE)
+    text_surface = font.render(text, True, YELLOW)
     text_rect = text_surface.get_rect()
     text_rect.midtop = (x, y)
     surf.blit(text_surface, text_rect)
